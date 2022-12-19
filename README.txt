503b05d97d24c4bbadb014b8108548436715e384
Balaji S <96479581+2005balaji@users.noreply.github.com>
2022-12-19 18:59:37 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,39 @@
+Cookery is simple meal finder application that can help people to decide their next meal by a simple click.
+
+about the application:- 
+
+There are many different meal finders available, ranging from simple search engines that allow users to enter specific ingredients or types of cuisine, to more comprehensive platforms that offer personalized recommendations based on users' past selections and preferences. Some meal finders also offer features such as shopping lists, step-by-step cooking instructions, and the ability to save and organize favorite recipes.
+
+One of the primary benefits of using a meal finder is the ability to quickly and easily find new meal ideas and recipes. Whether you're looking for a quick and easy dinner option, trying to stick to a specific diet, or just looking to try something new, a meal finder can help you find the perfect recipe to suit your needs
+
+
+How to open this File:-
+
+The  main file contains 4 files.
+
+1.README.md
+2.welcome.html
+3.welcome.gif
+3.data(folder)
+
+to view the output of the file. 
+step 1: download and extract.
+step 2: simply click on welcome.html, open on your preffered web-browser(Google Chrome:Recommended)
+
+incase the file is not opening plese click on the following URL : 
+
+
+
+
+any queries contact@balajisr000@gmail.com
+
+Credits:--
+Design: inspired from https://kalvium.community/
+references:-
+1.https://www.themealdb.com/api.php
+2.https://www.youtube.com/watch?v=tPqnKDBaMLQ 
+3.https://www.themealdb.com/
+4.https://www.w3schools.com/w3css/w3css_modal.asp
+
+
+
---
+++ data/script.js
@@ -0,0 +1,198 @@
+// getting elements and classes
+const searchBtn = document.getElementById("search-btn");
+const mealList = document.getElementById("meal");
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const recipeCloseBtn = document.getElementById("recipe-close-btn");
+const title = document.querySelector("#search");
+
+// event listeners
+searchBtn.addEventListener("click", getMealList);
+mealList.addEventListener("click", getMealRecipe);
+
+// close button
+recipeCloseBtn.addEventListener("click", () => {
+  mealDetailsContent.parentElement.classList.remove("showRecipe");
+  console.log("search meal was triggered  ");
+});
+
+// get meal list that matches with the ingredients
+function getMealList() {
+  // removing the spaces using .trim()
+  let searchInputTxt = document.getElementById("search-input").value.trim();
+  //removing the text Our Finds For Your Search
+  title.classList.remove("hidden");
+  //getting data from API
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchInputTxt}`
+  )
+    // promises
+    .then((response) => response.json())
+    .then((data) => {
+      let html1 = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          html1 += `
+            
+          
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                    <div class = "meal-img">
+                    <img src = "${meal.strMealThumb}" alt = "food">
+                    </div>                
+                    <div class = "meal-name">
+                    <h3>${meal.strMeal}</h3>
+                    <a href = "#" class = "recipe-btn">Recipe Ingredients</a>
+                    </div>
+                    </div>
+                `;
+        });
+        mealList.classList.remove("notFound");
+      } else if (searchInputTxt == "") {
+        html1 = "Enter Category Name/Type  !";
+        mealList.classList.add("notFound");
+      } else {
+        html1 = "Sorry, No meals found for the searched Category!";
+        mealList.classList.add("notFound");
+      }
+      // inputting data into Html container
+      mealList.innerHTML = html1;
+    });
+}
+
+// getting response to the funtions from API
+function getMealRecipe(ele) {
+  ele.preventDefault();
+  if (ele.target.classList.contains("recipe-btn")) {
+    let mealItem = ele.target.parentElement.parentElement;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      // promises
+      .then((response) => response.json())
+      .then((data) => mealRecipeModal(data.meals));
+  }
+}
+
+// modal output
+
+function mealRecipeModal(meal) {
+  console.log(meal);
+  // array indexing object has only 1 element
+  meal = meal[0];
+  let html3 = `
+  <h2 class = "recipe-title">${meal.strMeal}</h2> <br>
+  
+        <div class = "recipe-instruct">
+            <h3>Ingredients:- </h3>
+            <p>${meal.strIngredient1}<br>
+            ${meal.strIngredient2}<br>
+            ${meal.strIngredient3}<br>
+            ${meal.strIngredient4}<br>
+            ${meal.strIngredient5}<br>
+            ${meal.strIngredient6}<br>
+            ${meal.strIngredient7}<br>
+            ${meal.strIngredient8}<br>
+            ${meal.strIngredient9}<br>
+            ${meal.strIngredient10}<br>
+            </p>
+        </div>
+        <div class = "recipe-link">
+        <a href = "${meal.strYoutube}" target = "_blank">Click here to know More! </a>
+        </div>
+    `;
+  mealDetailsContent.innerHTML = html3;
+  // Show recipe or Ingredient Button
+  mealDetailsContent.parentElement.classList.add("showRecipe");
+}
+
+// random  meal function
+
+const mealList$random = document.getElementById("meal-random");
+
+const mealDetailsContent$random = document.querySelector(
+  ".meal-details-content-random"
+);
+
+const abc = document.querySelector(".meal-details");
+
+const recipeCloseBtn$random = document.getElementById(
+  "random-recipe-close-btn"
+);
+
+recipeCloseBtn.onclick = () => {
+  console.log("licked!");
+  abc.classList.remove('"random-showRecipe"');
+};
+
+//////////////////
+// random funtion
+
+function random() {
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    //promises
+    .then((response) => response.json())
+    .then((data) => {
+      let htmlr = "";
+      if (data.meals) {
+        console.log("Data loading from random function");
+        console.log(data);
+        data.meals.forEach((meal) => {
+          htmlr += `       
+                      <h1>Our Suggested Meal for your day</h1>
+                      <div class = "meal-item-random" data-id = "${meal.idMeal}">
+                      <div class = "meal-img-random">
+                      <img src = "${meal.strMealThumb}" alt = "food"></div>
+                      <div class = "meal-name-random">
+                      <h3>${meal.strMeal}</h3>
+                      <a id="recipe-btn-random" class = "recipe-btn-random">Get Ingredients</a></div>
+                      </div>
+                `;
+        });
+      }
+
+      mealList$random.innerHTML = htmlr;
+      // event listeners
+
+      let html2 = `
+          
+        
+                <h2 class = "recipe-title-random">${data.meals[0].strMeal}</h2>
+                <div class = "recipe-instruct-random">
+                    <h3>INGREDIENTS:</h3>
+                    <p>${data.meals[0].strIngredient1}<br>
+                    ${data.meals[0].strIngredient2}<br>
+                    ${data.meals[0].strIngredient3}<br>
+                    ${data.meals[0].strIngredient4}<br>
+                    ${data.meals[0].strIngredient5}<br>
+                    ${data.meals[0].strIngredient6}<br>
+                    ${data.meals[0].strIngredient7}<br>
+                    ${data.meals[0].strIngredient8}<br>
+                    ${data.meals[0].strIngredient9}<br>
+                    ${data.meals[0].strIngredient10}<br>           
+                    </p>
+                </div>
+                <div class = "recipe-link-random">
+                <a href = "${data.meals[0].strYoutube}" target = "_blank">Watch Video</a>
+                </div>
+            `;
+
+      mealDetailsContent$random.innerHTML = html2;
+
+      //displaying the in Ingredient Modal
+      document.getElementById("recipe-btn-random").onclick = () => {
+        mealDetailsContent$random.parentElement.classList.add(
+          "random-showRecipe"
+        );
+      };
+    });
+}
+
+// closing the opened modal (CSS)
+recipeCloseBtn$random.addEventListener("click", () => {
+  mealDetailsContent$random.parentElement.classList.remove("random-showRecipe");
+  console.log("Close Moadal");
+});
+
+// starting the random function
+window.onload = () => {
+  random();
+};
